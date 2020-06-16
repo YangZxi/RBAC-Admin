3a1de514f2037b8caa1e24c37b4c0775dfc6e70f
Young <yangzx1282@qq.com>
2020-06-16 23:08:56 +0800

    使用内存存储登录用户信息，模拟Redis

---
+++ src/main/java/cn/xiaosm/plainadmin/utils/MemoryUtils.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright: 2019-2020，小树苗(www.xiaosm.cn)
+ * FileName: MemoryUtils
+ * Author:   Young
+ * Date:     2020/6/16 21:31
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * Young         修改时间           版本号             描述
+ */
+package cn.xiaosm.plainadmin.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈〉
+ *
+ * @author Young
+ * @create 2020/6/16
+ * @since 1.0.0
+ */
+public class MemoryUtils {
+
+    private static HashMap<String, Object> map = new HashMap<>();
+
+    public static void saveObject(String key, Object o) {
+        map.put(key, o);
+    }
+
+    public static Object getObject(String key) {
+        return map.get(key);
+    }
+
+}
